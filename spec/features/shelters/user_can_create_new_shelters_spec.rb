6871e456bdc1b9621d7244410fb43697e728dc43
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-11-19 15:35:07 -0700

    Add final assertion for user story 4

--- spec/features/shelters/user_can_create_new_shelters_spec.rb
+++ spec/features/shelters/user_can_create_new_shelters_spec.rb
@@ -25,11 +25,18 @@ RSpec.describe "shelter creation page", type: :feature do
     expect(page).to have_field('shelter[zip]')
   end
 
-  # it "can send POST request to '/shelters', a new shelter is created, be redirected to Shelter Index page where it can see the new Shelter listed" do
-  #   fill_in 'Name', with: "Ridiculous Test Name"
-  #   fill_in 'Address', with: "124 Fake Ln."
-  #   fill_in 'City', with: "Faketown"
-  #   fill_in 'State', with: "FK"
-  #   fill_in 'Zip', with: "55555"
-  # end
+  it "can send POST request to '/shelters', a new shelter is created, be redirected to Shelter Index page where it can see the new Shelter listed" do
+    visit 'shelters/new'
+    
+    fill_in 'shelter[name]', with: "Ridiculous Test Name 2"
+    fill_in 'shelter[address]', with: "125 Fake Ln."
+    fill_in 'shelter[city]', with: "Faketown"
+    fill_in 'shelter[state]', with: "FK"
+    fill_in 'shelter[zip]', with: "55555"
+
+    click_button 'Submit'
+
+    expect(current_path).to eq('/shelters')
+    expect(page).to have_content("Ridiculous Test Name 2")
+  end
 end
