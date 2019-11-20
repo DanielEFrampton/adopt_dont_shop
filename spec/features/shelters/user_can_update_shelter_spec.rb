f3d6cd0938ddf33a31829b7e1613053b91f87730
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-11-19 17:02:16 -0700

    Add test for link taking user to edit page

--- spec/features/shelters/user_can_update_shelter_spec.rb
+++ spec/features/shelters/user_can_update_shelter_spec.rb
@@ -1,22 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe "shelter update process", type: :feature do
-  it "can see link on show page to update the shelter" do
+  before(:each) do
     shelter_1 = Shelter.create(id: "1",
                                name: "Ridiculous Test Name",
                                address: "124 Fake Ln.",
                                city: "Faketown",
                                state: "FK",
                                zip: "55555")
+  end
 
+  it "can see link on show page to update the shelter" do
     visit '/shelters/1'
 
     expect(page).to have_link('Edit Shelter')
   end
 
-  # it "can take user to '/shelters/:id/edit'" do
-  # end
-  #
+  it "can take user to '/shelters/:id/edit'" do
+    visit '/shelters/1'
+    click_link 'Edit Shelter'
+
+    expect(current_path).to eq('/shelters/1/edit')
+  end
+
   # it "can see form to edit shelter's data" do
   # end
   #
