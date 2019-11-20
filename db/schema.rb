ad2d66c06a5e7b32977231fdaee6d022cefc0475
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-11-20 15:03:19 -0700

    Update schema after Pet table migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191119041943) do
+ActiveRecord::Schema.define(version: 20191120220122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pets", force: :cascade do |t|
+    t.string "name"
+    t.string "sex"
+    t.string "image_path"
+    t.string "approx_age"
+    t.bigint "shelter_id"
+    t.index ["shelter_id"], name: "index_pets_on_shelter_id"
+  end
 
   create_table "shelters", force: :cascade do |t|
     t.string "name"
@@ -25,4 +34,5 @@ ActiveRecord::Schema.define(version: 20191119041943) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "pets", "shelters"
 end
