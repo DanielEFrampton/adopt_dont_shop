3c9b63e68f685310a8ccbbc59c3494080ef291eb
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-11-20 21:16:03 -0700

    Add feature test for Pets show view

---
+++ spec/features/pets/user_can_see_pet_with_id_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe "pet show page", type: :feature do
+  before(:each) do
+    @shelter_1 = Shelter.create(name: "Ridiculous Test Name",
+                               address: "124 Fake Ln.",
+                               city: "Faketown",
+                               state: "FK",
+                               zip: "55555")
+    @pet_1 = @shelter_1.pets.create({
+                                     name: "Bill",
+                                     approx_age: 3,
+                                     sex: "male",
+                                     image_path: "image1.png"
+                                   })
+    visit "/pets/#{@pet_1.id}"
+  end
+
+  it "can display pet name with given id" do
+    expect(page).to have_content(@pet_1.name)
+  end
+
+  xit "can display pet image" do
+    expect(page).to have_xpath("//img[@src='#{@pet_1.image_path}']")
+  end
+
+  xit "can display pet description" do
+    expect(page).to have_content("Description: #{@pet_1.name}")
+  end
+
+  xit "can display approximate age of pet" do
+    expect(page).to have_content("Approximate Age: #{@pet_1.approx_age}")
+  end
+
+  xit "can display gender of pet" do
+    expect(page).to have_content("Sex: #{@pet_1.sex}")
+  end
+
+  xit "can display adoptable status of pet" do
+    expect(page).to have_content("Adoptable Status: #{@pet_1.status}")
+  end
+end
