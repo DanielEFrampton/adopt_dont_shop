a9f72cdbe4cb1ceddb442fa136137da27b4334d2
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-11-21 00:16:53 -0700

    Add create method on Pets controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -10,4 +10,17 @@ class PetsController < ApplicationController
   def new
     @shelter = Shelter.find(params[:id])
   end
+
+  def create
+    shelter = Shelter.find(params[:id])
+    pet = shelter.pets.create({
+                                name:         params[:pet][:name],
+                                approx_age:   params[:pet][:approx_age],
+                                sex:          params[:pet][:sex],
+                                image_path:   params[:pet][:image_path],
+                                description:  params[:pet][:description]
+                              })
+    pet.save
+    redirect_to "/shelters/#{shelter.id}/pets"
+  end
 end
