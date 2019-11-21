adda8ebf6698390039c62e277ec08b60b585b3a9
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-11-20 21:30:54 -0700

    Update schema after db migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191121042524) do
+ActiveRecord::Schema.define(version: 20191121043014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20191121042524) do
     t.string "approx_age"
     t.bigint "shelter_id"
     t.boolean "adoptable", default: true
+    t.string "description"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
