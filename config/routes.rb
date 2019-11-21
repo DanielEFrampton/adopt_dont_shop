2d006c28b2ce8b296834f4f2a50575614e653c7e
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-11-21 14:51:02 -0700

    Add route for pet update

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,5 @@ Rails.application.routes.draw do
   post '/shelters/:id/pets', to: 'pets#create'
   get '/shelters/:id/pets/new', to: 'pets#new'
   get 'pets/:id/edit', to: 'pets#edit'
+  patch 'pets/:id', to: 'pets#update'
 end
