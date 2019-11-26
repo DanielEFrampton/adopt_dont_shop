70a8bbd9223f6c7933c75d3a7e135eafa6dc273b
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-11-26 13:08:22 -0700

    Add model test for Pet model method required by feature test

--- spec/models/pets_spec.rb
+++ spec/models/pets_spec.rb
@@ -17,5 +17,36 @@ describe Pet, type: :model do
   end
 
   describe "methods" do
+    before(:each) do
+      @shelter_1 = Shelter.create(name: "Ridiculous Test Name",
+                                 address: "124 Fake Ln.",
+                                 city: "Faketown",
+                                 state: "FK",
+                                 zip: "55555")
+      @pet_1 = @shelter_1.pets.create({
+                                   name: "Bill",
+                                   approx_age: 3,
+                                   sex: "male",
+                                   image_path: "http://www.thing.com/image.png",
+                                   description: "Very canine",
+                                   adoptable: true
+                                 })
+      @pet_2 = @shelter_1.pets.create({
+                                   name: "Jill",
+                                   approx_age: 5,
+                                   sex: "female",
+                                   image_path: "http://www.thing.com/image.png",
+                                   description: "Very canine",
+                                   adoptable: false
+                                 })
+    end
+
+    it "can generate appropriate arguments for adoption status change link" do
+      expected_result_1 = ['Change to Adoption Pending', "/pets/#{@pet_1.id}/pending", method: :patch]
+      expect(@pet_1.adoptable_link_args).to eq(expected_result_1)
+
+      expected_result_2 = ['Change to Adoptable', "/pets/#{@pet_2.id}/adoptable", method: :patch]
+      expect(@pet_2.adoptable_link_args).to eq(expected_result_2)
+    end
   end
 end
