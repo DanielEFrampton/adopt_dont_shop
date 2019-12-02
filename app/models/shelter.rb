8d17161c535c2fae89b256474dd05bbb4d3b418f
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-12-02 11:34:29 -0700

    Remove presentation logic from Pet model, refactor as wrapper for data

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -2,7 +2,7 @@ class Shelter < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip
   has_many :pets
 
-  def pet_count_label
-    "(#{pets.count} Pet#{"s" if pets.count != 1})"
+  def pet_count
+    pets.count
   end
 end
